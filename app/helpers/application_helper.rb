ba915d16f744de9a5dcd6cec540a63072cb8d9a2
Chris Brown <chrmbrow@gmail.com>
2014-11-11 14:34:48 -0500

    add bootstrap classes to flash messages

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,19 @@
 module ApplicationHelper
+  def flash_class(name)
+    alert_type = case name.to_s
+      when 'notice'
+        'alert-success'
+      when 'error'
+        'alert-danger'
+      when 'alert'
+        'alert-danger'
+      when 'info'
+        'alert-info'
+      when 'warning'
+        'alert-warning'
+    else
+      "alert-#{name}"
+    end
+    alert_type
+  end
 end
