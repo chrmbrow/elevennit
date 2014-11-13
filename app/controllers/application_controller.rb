35c9f18123a70c5c054cf14fe8ce14ac6d5792b3
Chris Brown <chrmbrow@gmail.com>
2014-11-12 20:05:09 -0500

    Use Devise for authentication.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,6 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
@@ -10,4 +12,12 @@ class ApplicationController < ActionController::Base
     # @categories = Category.all if !defined?(@categories)
     # @categories
   end
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.for(:sign_up) << :username
+    devise_parameter_sanitizer.for(:account_update) << :username
+  end
+
 end
