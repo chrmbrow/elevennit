36cde3d482f6840cd8aabfba32d0d4d7cb94f5e6
Chris Brown <chrmbrow@gmail.com>
2014-11-13 10:11:58 -0500

    Create threaded comments.

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,4 +1,15 @@
 class CommentsController < ApplicationController
+
+  def new
+    @parent = Comment.find params[:parent_id]
+    post = @parent.commentable
+    @comment = Comment.build_from(post, current_user.id, '')
+    @comment.parent_id = @parent.id
+    respond_to do |format|
+      format.js {}
+    end
+  end
+
   def create
     post = Post.find params[:comment][:commentable_id]
     comment = Comment.build_from post, current_user.id, comment_params[:body]
